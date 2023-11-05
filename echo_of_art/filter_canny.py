321834d553b39e95718c8df1c15b54b9b7356fd5
Yusuke Kawamoto <novogrammer@gmail.com>
2023-11-05 19:06:17 +0900

    filter_canny.pyのクラス化

--- echo_of_art/filter_canny.py
+++ echo_of_art/filter_canny.py
@@ -6,14 +6,18 @@ from runner import run
 from dotenv import load_dotenv
 import os
 
-
-def filter_canny(image_before:UMat)->UMat:
-  image_gray = cv2.cvtColor(image_before, cv2.COLOR_BGR2GRAY)
-  image_after = cv2.Canny(image_gray, 100, 200)
-  return image_after
+class FilterCanny:
+  def __init__(self) -> None:
+    pass
+  def __call__(self, image_before:UMat) -> UMat:
+    image_gray = cv2.cvtColor(image_before, cv2.COLOR_BGR2GRAY)
+    image_gray_canny = cv2.Canny(image_gray, 100, 200)
+    image_after = cv2.cvtColor(image_gray_canny, cv2.COLOR_GRAY2BGR)
+    return image_after
 
 
 if __name__ == '__main__':
+  filter=FilterCanny()
   load_dotenv()
   MY_PORT=int(os.getenv("EOA_ENTITY_A_PORT","5000"))
   YOUR_ADDRESS=os.getenv("EOA_ENTITY_B_ADDRESS","127.0.0.1")
@@ -23,4 +27,4 @@ if __name__ == '__main__':
   print(f"YOUR_ADDRESS: {YOUR_ADDRESS}")
   print(f"YOUR_PORT: {YOUR_PORT}")
 
-  run(filter_canny,MY_PORT,YOUR_ADDRESS,YOUR_PORT)
+  run(filter,MY_PORT,YOUR_ADDRESS,YOUR_PORT)
