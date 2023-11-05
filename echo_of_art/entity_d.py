2be80846eb29af4d354ada6c0e45ef6aaf307153
Yusuke Kawamoto <novogrammer@gmail.com>
2023-11-05 15:57:58 +0900

    entity_*.pyを追加、filter_noopを使って最低限ネットワーク負荷のテストができるようにした。

---
+++ echo_of_art/entity_d.py
@@ -0,0 +1,19 @@
+from cv2 import UMat
+
+from runner import run
+from dotenv import load_dotenv
+import os
+
+from filter_noop import filter_noop
+
+if __name__ == '__main__':
+  load_dotenv()
+  MY_PORT=int(os.getenv("EOA_ENTITY_D_PORT","5000"))
+  YOUR_ADDRESS=os.getenv("EOA_RECEIVER_ADDRESS","127.0.0.1")
+  YOUR_PORT=int(os.getenv("EOA_RECEIVER_PORT","5000"))
+
+  print(f"MY_PORT: {MY_PORT}")
+  print(f"YOUR_ADDRESS: {YOUR_ADDRESS}")
+  print(f"YOUR_PORT: {YOUR_PORT}")
+
+  run(filter_noop,MY_PORT,YOUR_ADDRESS,YOUR_PORT)
