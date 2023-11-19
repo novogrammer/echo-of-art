32177b90efade111f4980c97faa00965e34caa29
Yusuke Kawamoto <novogrammer@gmail.com>
2023-11-19 14:49:32 +0900

    どれかわからないのでindexとnameも追記

--- echo_of_art/_pyaudio_devicelist.py
+++ echo_of_art/_pyaudio_devicelist.py
@@ -3,4 +3,4 @@ import pyaudio
 audio = pyaudio.PyAudio()
 
 for i in range(0, audio.get_device_count()):
-    print(audio.get_device_info_by_index(i)['name'])
+    print(f"[{i}]['name']: {audio.get_device_info_by_index(i)['name']}")
